0ee59f07eabb08678a4c3135ba2c04691597e003
KevinGitari <Kevin>
2024-11-07 08:23:40 +0300

    lab

---
+++ server/migrations/versions/2059c9cbf6ce_message.py
@@ -0,0 +1,24 @@
+"""message
+
+Revision ID: 2059c9cbf6ce
+Revises: 
+Create Date: 2024-11-06 13:19:21.055056
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2059c9cbf6ce'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
